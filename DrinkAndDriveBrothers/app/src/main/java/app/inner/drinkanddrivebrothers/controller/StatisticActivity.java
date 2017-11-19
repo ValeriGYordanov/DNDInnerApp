cda203317578cfb47fbfd878544bdbf36dc9f883
ValeryJordanov <v.iordanov@paip3.com>
2017-11-19 20:53:47 +0200

    Daily Statistics added, new Starting Icon

--- DrinkAndDriveBrothers/app/src/main/java/app/inner/drinkanddrivebrothers/controller/StatisticActivity.java
+++ DrinkAndDriveBrothers/app/src/main/java/app/inner/drinkanddrivebrothers/controller/StatisticActivity.java
@@ -1,11 +1,18 @@
 package app.inner.drinkanddrivebrothers.controller;
 
+import android.content.DialogInterface;
+import android.graphics.Color;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Size;
+import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -26,6 +33,7 @@ public class StatisticActivity extends AppCompatActivity {
     private DatabaseReference ref;
     private RecyclerView statisticRecView;
     private StatisticsRecView recyclerViewAdapter;
+    private Button dailyStatistics;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +41,9 @@ public class StatisticActivity extends AppCompatActivity {
         setContentView(R.layout.activity_statistic);
 
         statisticRecView = findViewById(R.id.statistics_list);
+        dailyStatistics = findViewById(R.id.day_statistics);
+        dailyStatistics.setClickable(false);
+
         LinearLayoutManager recLayMan = new LinearLayoutManager(this);
         statisticRecView.setLayoutManager(recLayMan);
 
@@ -47,6 +58,47 @@ public class StatisticActivity extends AppCompatActivity {
         }else{
             getCourses(date);
         }
+
+        dailyStatistics.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                double dailyKM = 0;
+                double dailyMoney = 0;
+                for (Course crs : allCourses){
+                    dailyKM += crs.getKm();
+                    dailyMoney += crs.getPrice();
+                }
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(StatisticActivity.this);
+                builder.setTitle("Статистика за деня!");
+
+                LinearLayout layout = new LinearLayout(StatisticActivity.this);
+                layout.setOrientation(LinearLayout.VERTICAL);
+
+                final TextView allKilos = new TextView(StatisticActivity.this);
+                allKilos.setTextColor(Color.BLACK);
+                allKilos.setTextSize(15);
+                allKilos.setText("Изминати Километри за деня : " + Double.toString(dailyKM) + " км.");
+                layout.addView(allKilos);
+
+                final TextView allMoney = new TextView(StatisticActivity.this);
+                allMoney.setTextColor(Color.BLACK);
+                allMoney.setTextSize(15);
+                allMoney.setText("Оборот за деня : " + Double.toString(dailyMoney) + " лв.");
+                layout.addView(allMoney);
+
+                builder.setView(layout);
+                builder.setNegativeButton("Назад", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+
+                builder.show();
+            }
+        });
+
     }
 
     private void getCourses(String date) {
@@ -60,7 +112,7 @@ public class StatisticActivity extends AppCompatActivity {
                     Course course = data.getValue(Course.class);
                     allCourses.add(course);
                 }
-
+                dailyStatistics.setClickable(true);
                 recyclerViewAdapter = new StatisticsRecView(StatisticActivity.this, allCourses);
                 statisticRecView.setAdapter(recyclerViewAdapter);
             }
@@ -85,7 +137,7 @@ public class StatisticActivity extends AppCompatActivity {
                     Course course = data.getValue(Course.class);
                     allCourses.add(course);
                 }
-
+                dailyStatistics.setClickable(true);
                 recyclerViewAdapter = new StatisticsRecView(StatisticActivity.this, allCourses);
                 statisticRecView.setAdapter(recyclerViewAdapter);
             }
